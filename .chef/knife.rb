dd3e6ad85aa8dc57e3e76dcfdd1905d6cb958976
Tim Park <timfpark@gmail.com>
2014-07-19 20:16:24 -0700

    remove last chef vestige

--- .chef/knife.rb
+++
@@ -1,9 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'timpark'
-client_key               '/Users/timpark/dev/nitrogen/devops/.chef/timpark.pem'
-validation_client_name   'chef-validator'
-validation_key           '/Users/timpark/dev/nitrogen/devops/.cheef/chef-validator.pem'
-chef_server_url          'https://n2chef.cloudapp.net:443'
-syntax_check_cache_path  '/Users/timpark/dev/nitrogen/devops/.chef/syntax_check_cache'
-cookbook_path            [ 'cookbooks' ]
